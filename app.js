709e5410dae1f903aecd68cbbb35c2a1eebf35d2
Karthik <kbalasub@stevens.edu>
2021-07-14 22:50:47 -0500

    Simple and Nested Object

--- app.js
+++ app.js
@@ -6,3 +6,28 @@ var number1 = '7';
 var number2 = 7;
 var result = add(+number1, number2);
 console.log('Result: ', result);
+//object with explict types
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Karu',
+//     age: 30
+// }
+//simple object
+var person = {
+    name: 'Karu',
+    age: 30
+};
+console.log(person.name + ' is ' + person.age);
+//nested object
+var product = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+};
+console.log(typeof product);
--- app.ts
+++ app.ts
@@ -9,3 +9,33 @@ const number2 = 7
 const result = add(+number1, number2)
 
 console.log('Result: ', result)
+
+//object with explict types
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Karu',
+//     age: 30
+// }
+
+//simple object
+const person = {
+    name: 'Karu',
+    age: 30
+}
+
+console.log(person.name + ' is ' + person.age)
+
+//nested object
+const product = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+}
+
+console.log(typeof product)
