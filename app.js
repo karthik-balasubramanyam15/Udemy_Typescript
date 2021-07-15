3bc4793b30c18556fae39556b16218d8fcc4a11d
Karthik <kbalasub@stevens.edu>
2021-07-14 22:15:43 -0500

    Learning basics of typecasting

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Code goes here!!');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = '7';
+var number2 = 7;
+var result = add(+number1, number2);
+console.log('Result: ', result);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Code goes here!!')
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const number1 = '7'
+const number2 = 7
+
+const result = add(+number1, number2)
+
+console.log('Result: ', result)
